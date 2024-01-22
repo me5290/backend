a9b54f5da66815548977868d180fef911a8e8964
me5290 <me5290@naver.com>
2024-01-22 23:20:03 +0900

    백준

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String[] array = input.split(" ");
-        String A = array[0];
-        String B = array[1];
-        String C = array[2];
-        System.out.println(Integer.parseInt(A)+Integer.parseInt(B)+Integer.parseInt(C));
+        String time = sc.nextLine();
+        String[] time2 = time.split(" ");
+        int hour = Integer.parseInt(time2[0]);
+        int min = Integer.parseInt(time2[1]);
+        if (min-45 < 0){
+            if(hour == 0){
+                hour = 24;
+            }
+            min = 60 - (45-min);
+            System.out.println((hour-1)+" "+min);
+        }else {
+            System.out.println(hour+" "+(min-45));
+        }
     }
 }
