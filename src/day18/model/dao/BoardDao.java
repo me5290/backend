18420e764575f5205464755962ece009b281b2ca
KDH <lococo0425@naver.com>
2024-01-25 16:13:32 +0900

    day18

--- src/day18/model/dao/BoardDao.java
+++ src/day18/model/dao/BoardDao.java
@@ -1,4 +1,9 @@
 package day18.model.dao;
 
-public class BoardDao {
+public class BoardDao extends Dao{
+    private BoardDao(){}
+    private static BoardDao boardDao = new BoardDao();
+    public static BoardDao getInstance(){
+        return boardDao;
+    }
 }
--- src/day18/model/dao/MemberDao.java
+++ src/day18/model/dao/MemberDao.java
@@ -8,29 +8,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class MemberDao {
-    private Connection con; // 1. DB 연동객체
-    private PreparedStatement ps; // 2. 작성된 SQL 가지고 있고, 실행 담당
-    private ResultSet rs; // 3. SQL 실행 결과
-
+public class MemberDao extends Dao{
     // 싱글톤
-    private MemberDao(){    // 생성자 : 객체 생성시 초기화 담당
-        // - 객체 생성시 DB연동
-        try {
-            // 1. MYSQL 회사의 JDBC관련된 (Driver)객체를 JVM에 로딩한다 , 불러오기
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // 2. 연동된 결과의 (구현체)객체를 Connection 인터페이스에 대입한다
-            con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/java",
-                    "root",
-                    "1234"
-            );
-            System.out.println("<DB연동 성공>");
-        }catch (Exception e){
-            System.out.println("<DB연동 실패>"+e);
-        }
-    }
+    private MemberDao(){}
     private static MemberDao memberDao = new MemberDao();
     public static MemberDao getInstance(){
         return memberDao;
